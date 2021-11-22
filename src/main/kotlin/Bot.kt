aca7aecad5a711a275dade419ab807583f3bd375
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-11-22 17:00:08 +0000

    Add ability to select users to greet. Handled if user is not in a voice channel when @'ing the bot.

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,14 +1,12 @@
-import LavaAudioPlayer.AudioPlayerSendHandler
-import LavaAudioPlayer.TrackScheduler
-import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager
-import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers
 import dev.kord.common.annotation.KordVoice
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
 import dev.kord.core.any
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.entity.Message
 import dev.kord.core.entity.channel.VoiceChannel
 import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.exception.EntityNotFoundException
 import dev.kord.core.on
 import dev.kord.rest.NamedFile
 import dev.kord.rest.builder.message.create.UserMessageCreateBuilder
@@ -17,28 +15,27 @@ import dev.kord.voice.VoiceConnection
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.io.InputStream
 import java.net.URL
 
 
 // Maybe dictionary of enum to method to enact would work better?
 enum class GreetingsType {
-    GREET_SENDER, GREET_MENTIONED_USERS, GREET_ALL, NONE
+    GREET_SENDER, GREET_MENTIONED_USERS, NONE
 }
 @OptIn(KordVoice::class)
 class Bot(private val client: Kord, greetingsVideoURL: URL, greetingsAudioURL: URL, audioPlayer: IAudioPlayer) {
-    // If message == @greetinsbot 'greet' then play generic greetings video
+    // If message == @greetingsbot 'greet' then play generic greetings video
     // if message contains greetings at member, then greet specific member
     // if message == "@greetingsbot join and greet"
     private val personalGreetings = arrayOf("hello", "greet me", "greetings", "greet")
     private val otherUserGreetings = arrayOf("greet them", "greet", "say hello to")
-    private val allGreetings = "greet all"
+    private val botId = client.selfId
 
     init {
         client.on<MessageCreateEvent> {
             val users = message.mentionedUsers
-            if (!users.any { x -> x.isBot }) return@on
+            if (!users.any { x -> x.id == botId }) return@on
             val greetingsType = parseGreetingsType(message)
 
             launch(Dispatchers.IO)
@@ -48,23 +45,27 @@ class Bot(private val client: Kord, greetingsVideoURL: URL, greetingsAudioURL: U
                     createGreetingsMessage(this, videoInputStream, greetingsType, this@on)
                 }
 
-                val userVoiceChannel = message.getAuthorAsMember()?.getVoiceState()?.channelId!!
-                val channel = client.getChannelOf<VoiceChannel>(userVoiceChannel)!!
+                try {
+                    message.getAuthorAsMember()?.getVoiceState()?.channelId?.let { snowflake ->
+                        val channel = client.getChannelOf<VoiceChannel>(snowflake) ?: return@let
+                        val connection: VoiceConnection?
 
-                val connection: VoiceConnection?
+                        audioPlayer.loadLocalSoundFile(greetingsAudioURL.path)
 
-                audioPlayer.loadLocalSoundFile(greetingsAudioURL.path)
+                        connection = channel.connect {
+                            audioProvider { AudioFrame.fromData(audioPlayer.audioData) }
+                        }
 
-                connection = channel.connect {
-                    audioProvider { AudioFrame.fromData(audioPlayer.audioData) }
-                }
-
-                launch {
-                    audioPlayer.onPlayerEvent.collect {
-                        when(it) {
-                            PlayerEventType.FINISH -> connection.leave()
+                        launch {
+                            audioPlayer.onPlayerEvent.collect {
+                                when(it) {
+                                    PlayerEventType.FINISH -> connection.leave()
+                                }
+                            }
                         }
                     }
+                } catch (ex: EntityNotFoundException) {
+                    println("User not in voice channel, skipping voice connection")
                 }
             }
         }
@@ -77,27 +78,33 @@ class Bot(private val client: Kord, greetingsVideoURL: URL, greetingsAudioURL: U
         messageCreateEvent: MessageCreateEvent
     ) {
         userMessageCreateBuilder.files.add(0, NamedFile("greetings.mp4", videoInputStream))
-        when (greetingsType) {
-            GreetingsType.GREET_SENDER -> {
-                userMessageCreateBuilder.content = "GREETINGS ${messageCreateEvent.message.author?.mention}!"
-            }
-            GreetingsType.GREET_MENTIONED_USERS -> "GREETINGS "
-            GreetingsType.GREET_ALL -> TODO()
-            GreetingsType.NONE -> TODO()
+        val msg = when (greetingsType) {
+            GreetingsType.GREET_SENDER -> "GREETINGS ${messageCreateEvent.message.author?.mention}!"
+            GreetingsType.GREET_MENTIONED_USERS -> "GREETINGS ${messageCreateEvent.message.mentionedUserIds.toMentionedUsersMessage()}!"
+            GreetingsType.NONE -> ""
         }
+        userMessageCreateBuilder.content = msg
+    }
+
+    private fun Set<Snowflake>.toMentionedUsersMessage(): String {
+        val userMentions = mutableListOf<String>()
+        this.forEach {
+            if (it.value == botId.value) return@forEach
+            userMentions.add("<@${it.value}>")
+        }
+        return userMentions.joinToString()
     }
 
     private suspend fun parseGreetingsType(message: Message): GreetingsType {
         val msgContentLower = message.content.lowercase().replace(Regex("<.*?>"), "").trim()
 
-        if (msgContentLower == allGreetings) return GreetingsType.GREET_ALL
         // if mentions contain any other users AND message content matches expected greetings text
         if (message.mentionedUsers.any { user -> !user.isBot }
             && otherUserGreetings.any{greetings -> msgContentLower == greetings}) {
             return GreetingsType.GREET_MENTIONED_USERS
         }
         if (personalGreetings.any{greetings -> msgContentLower == greetings}) return GreetingsType.GREET_SENDER
-        return GreetingsType.NONE
+        return GreetingsType.GREET_SENDER
     }
 
     suspend fun start() {
